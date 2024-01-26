bc2550333544a51c25d1a92b476699feb38ba3f0
Kevin Mulcrone <kaybesee@gmail.com>
2024-01-25 21:37:50 -0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,116 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+
+function getRandomOption(options: Option[]): Option | null {
+  if (options.length === 0) {
+    return null;
+  }
+
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
+}
+
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const Loading = () => (
+  <div className="animate-pulse flex flex-1">
+    <div className="rounded-full bg-slate-500 h-9 w-full"></div>
+  </div>
+);
+
+type Gametype =
+  | "Team Slayer"
+  | "Capture the Flag"
+  | "King of the Hill"
+  | "Oddball";
+
+type Map =
+  | "Guardian"
+  | "Narrows"
+  | "The Pit"
+  | "Construct"
+  | "Heretic"
+  | "Amplified"
+  | "Onslaught";
+
+interface Option {
+  gametype: Gametype;
+  map: Map;
+}
+
+const options: Option[] = [
+  { gametype: "Team Slayer", map: "Amplified" },
+  { gametype: "Team Slayer", map: "Construct" },
+  { gametype: "Team Slayer", map: "Narrows" },
+  { gametype: "Team Slayer", map: "The Pit" },
+  { gametype: "Oddball", map: "Guardian" },
+  { gametype: "King of the Hill", map: "Construct" },
+  { gametype: "Capture the Flag", map: "Onslaught" },
+  { gametype: "Capture the Flag", map: "Heretic" },
+  { gametype: "Capture the Flag", map: "Narrows" },
+  { gametype: "Capture the Flag", map: "The Pit" },
+];
 
 export default function Home() {
+  const [current, setCurrent] = useState(getRandomOption(options));
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="py-24 bg-gradient-to-br from-[#0F1822] from-0% via-[#31414F] via-80% to-[#EDBF5A] to-100% sm:py-32 h-screen flex items-center">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-base font-semibold leading-7 text-[#9DC656]">
+            Halo 3
+          </h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            Dump Train
+          </p>
+          <p className="mt-2 text-lg leading-8 text-gray-400">
+            Do you want dump, or you want train?
+          </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-8 lg:max-w-4xl">
+          <dl className="grid max-w-xl grid-cols-1 gap-y-10 lg:max-w-none lg:grid-cols-5 lg:gap-y-16">
+            <div className="relative col-span-2">
+              <dd className="mt-2 leading-7 text-white text-4xl font-semibold text-center">
+                {isLoading ? <Loading /> : current?.gametype}
+              </dd>
+            </div>
+            <div className="relative col-span-1">
+              <dd className="mt-2 leading-7 text-gray-200 text-lg font-semibold text-center">
+                on
+              </dd>
+            </div>
+
+            <div className="relative col-span-2">
+              <dd className="mt-2 leading-7 text-white text-4xl font-semibold text-center">
+                {isLoading ? <Loading /> : current?.map}
+              </dd>
+            </div>
+          </dl>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl w-full text-center">
+          <button
+            type="button"
+            className="mx-auto rounded-md bg-[#F9D97A] px-7 py-2.5 text-sm font-semibold text-gray-800 shadow-sm hover:bg-[#FBE4A1] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={async () => {
+              if (!isLoading) {
+                console.log("runs");
+                setIsLoading(true);
+                await sleep(5000);
+                const option = getRandomOption(options);
+                setCurrent(option);
+                setIsLoading(false);
+              }
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Pick Game
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
